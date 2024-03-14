32bc6c38bd759fcbccf481e14ba386bfc46d9b32
Marcos Jesus <marcos.jesus@weduu.com>
2024-03-14 18:11:44 -0300

    Fix e adição de alerta ao criar um usuario e pré-modelamento de dash

--- app/static/js/scripts.js
+++ app/static/js/scripts.js
@@ -6,9 +6,7 @@
 //
 // Scripts
 // 
-
 window.addEventListener('DOMContentLoaded', event => {
-
     // Navbar shrink function
     var navbarShrink = function () {
         const navbarCollapsible = document.body.querySelector('#mainNav');
@@ -16,11 +14,10 @@ window.addEventListener('DOMContentLoaded', event => {
             return;
         }
         if (window.scrollY === 0) {
-            navbarCollapsible.classList.remove('navbar-shrink')
+            navbarCollapsible.classList.remove('navbar-shrink');
         } else {
-            navbarCollapsible.classList.add('navbar-shrink')
+            navbarCollapsible.classList.add('navbar-shrink');
         }
-
     };
 
     // Shrink the navbar 
@@ -36,7 +33,7 @@ window.addEventListener('DOMContentLoaded', event => {
             target: '#mainNav',
             rootMargin: '0px 0px -40%',
         });
-    };
+    }
 
     // Collapse responsive navbar when toggler is visible
     const navbarToggler = document.body.querySelector('.navbar-toggler');
@@ -50,5 +47,56 @@ window.addEventListener('DOMContentLoaded', event => {
             }
         });
     });
+});
+
+window.addEventListener('DOMContentLoaded', event => {
+    // Toggle the side navigation
+    const sidebarToggle = document.body.querySelector('#sidebarToggle');
+    if (sidebarToggle) {
+        // Uncomment Below to persist sidebar toggle between refreshes
+        // if (localStorage.getItem('sb|sidebar-toggle') === 'true') {
+        //     document.body.classList.toggle('sb-sidenav-toggled');
+        // }
+        sidebarToggle.addEventListener('click', event => {
+            event.preventDefault();
+            document.body.classList.toggle('sb-sidenav-toggled');
+            localStorage.setItem('sb|sidebar-toggle', document.body.classList.contains('sb-sidenav-toggled'));
+        });
+    }
 
 });
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const createAccountBtn = document.getElementById('createAccountBtn');
+    if (createAccountBtn) {
+        document.getElementById('createAccountBtn').addEventListener('click', function(event) {
+            event.preventDefault(); // Impede o envio do formulário antes do processamento
+        
+        // Envia o formulário via AJAX
+        fetch('/register', {
+            method: 'POST',
+            body: new FormData(document.querySelector('form'))
+        })
+        .then(response => response.json())
+        .then(data => {
+            // Verifica se o registro foi bem-sucedido
+            if (data.success) {
+                // Exibe o modal de sucesso
+                $('#successModal').modal('show');
+                
+                // Redireciona para a página de login após um curto período de tempo
+                setTimeout(function() {
+                    window.location.href = "login";
+                }, 3000);
+            } else {
+                // Exibe mensagens de erro, se houver
+                data.errors.forEach(error => {
+                    alert(error);
+                });
+            }
+        })
+        .catch(error => console.error('Erro:', error));
+    });
+}});
+
--- app/views/auth.py
+++ app/views/auth.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, render_template, request, redirect, url_for, session
+from flask import Blueprint, render_template, request, redirect, url_for, session, flash
 from app.models.models import db, User
+from flask import jsonify
 
 auth_bp = Blueprint('auth', __name__)
 
@@ -8,27 +9,20 @@ def login():
     if request.method == 'POST':
         email = request.form.get('email')
         password = request.form.get('password')
-
-        # Verifique se o email e a senha estão presentes no banco de dados
         user = User.query.filter_by(email=email, password=password).first()
 
         if user:
-            # Se o usuário existir, defina uma variável de sessão para indicar que o usuário está logado
             session['user_id'] = user.id
-
-            # Redirecione para a página do dashboard
             return redirect(url_for('dashboard.dashboard'))
-
         else:
-            # Se as credenciais estiverem incorretas, exiba uma mensagem de erro
-            return 'Credenciais inválidas. Por favor, tente novamente.'
+            
+            return redirect(url_for('auth.login'))
 
     return render_template('login.html')
 
 @auth_bp.route('/register', methods=['GET','POST'])
 def register():
     if request.method == 'POST':
-        # Obtenha os dados do formulário
         first_name = request.form.get('first_name')
         last_name = request.form.get('last_name')
         email = request.form.get('email')
@@ -36,21 +30,15 @@ def register():
         password_confirm = request.form.get('password_confirm')
 
         if first_name and last_name and email and password and password_confirm:
-            # Crie um novo usuário com os dados do formulário
             new_user = User(first_name=first_name, last_name=last_name, email=email, password=password)
-
-            # Adicione o novo usuário ao banco de dados
             db.session.add(new_user)
             db.session.commit()
 
-            print(f'Nome: {first_name} {last_name}')
-            print(f'E-mail: {email}')
-            print(f'Senha: {password}')
-            print(f'Confirmação de senha: {password_confirm}')
+            # Retorna uma resposta JSON indicando sucesso
+            return jsonify({"success": True})
 
-            # Redirecione para uma página de confirmação ou outra página
-            return redirect(url_for('auth.login'))
         else:
-            return 'Por favor, preencha todos os campos do formulário.'
+            # Retorna uma resposta JSON indicando falha e as mensagens de erro
+            return jsonify({"success": False, "errors": ["Por favor, preencha todos os campos do formulário."]}), 400
 
     return render_template('register.html')
